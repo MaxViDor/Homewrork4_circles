7695a6a76b709e299bce0e575bea19c16c351430
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-25 13:19:38 +0300

    add 3 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2 ();
+        task3 ();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i =1; i < 11; i = i +1) {
+            System.out.println("Итерация цикла" + i );
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i = i-1) {
+            System.out.println("Обратная итерация " + i);
+        }
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("Итерация четных чисел " + i);
+        }
     }
 }
