1f673542bfdcb0fd91d4c874868a4d1d656d5302
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-25 15:03:57 +0300

    add 7 task

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task4 ();
         task5 ();
         task6 ();
+        task7 ();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -40,5 +41,10 @@ public class Main {
         System.out.println("Задача 6");
         for (int i = 7; i <= 98; i = i + 7)
             System.out.println("Итерация чисел " +i);
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2)
+            System.out.println("Итерация чисел с умножением " + i);
     }
 }
